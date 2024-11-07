0e07e08005644de0adb0341b8faa93b0a537bcd8
FelipePinheiro <felipe.afp27@gmail.com>
2024-11-06 22:40:40 -0300

    Header layout adjustments

--- anota-system/src/Components/header/Header.tsx
+++ anota-system/src/Components/header/Header.tsx
@@ -1,15 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import { Box, List, ListItem } from "@mui/material";
 import LogoHeader from "../../LogoHeader.svg";
 import ButtonDropDown from "../buttonDropdown/ButtonDropDown";
 
 const Header = () => {
+  const [activeItem, setActiveItem] = useState("Agendamentos");
+
+  const handleItemClick = (item: string) => {
+    setActiveItem(item);
+  };
+
   return (
     <Box
       display="flex"
       justifyContent="space-between"
       alignItems="center"
+      sx={{ background: "#fff", boxShadow: "rgba(0, 0, 0, 0.24) 0px 3px 8px" }}
       paddingTop={2}
+      paddingBottom={2}
       paddingRight={4}
       paddingLeft={6}
     >
@@ -19,10 +27,25 @@ const Header = () => {
         style={{ width: "40px", height: "40px" }}
       />
       <Box>
-        <List disablePadding sx={{ display: "flex", gap: 2 }}>
-          <ListItem sx={{ width: "auto", padding: 0 }}>Agendamentos</ListItem>
-          <ListItem sx={{ width: "auto", padding: 0 }}>Quadras</ListItem>
-          <ListItem sx={{ width: "auto", padding: 0 }}>Layout</ListItem>
+        <List disablePadding sx={{ display: "flex", gap: 15 }}>
+          {["Agendamentos", "Quadras", "Layout"].map((item) => (
+            <ListItem
+              key={item}
+              sx={{
+                cursor: "pointer",
+                borderBottom:
+                  activeItem === item
+                    ? "2px solid #2AA137"
+                    : "1px solid transparent",
+                "&:hover": {
+                  opacity: 0.8,
+                },
+              }}
+              onClick={() => handleItemClick(item)}
+            >
+              {item}
+            </ListItem>
+          ))}
         </List>
       </Box>
       <ButtonDropDown />
